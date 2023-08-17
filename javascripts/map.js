dbd7ec236fa81267d4b220b3269ddcf0342672dd
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-08-17 14:13:28 +0900

    [Feat] 지도에서의 HTML, SCC

--- javascripts/map.js
+++ javascripts/map.js
@@ -7,7 +7,16 @@ async function initMap() {
   map = new Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
+    zoomControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false
   });
 }
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
 initMap();
